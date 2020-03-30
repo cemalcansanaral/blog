0b7fd73a7988d3f9575de494d2bb4be018aa9dec
Cemal Can Şanaral <c.can.sanaral@gmail.com>
2020-03-30 16:43:41 +0300

    Updates

---
+++ precache-manifest.fef9295f476e5111aa8d4c5e1443725d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f45f7b5a18e32701ccfa9bbeff26ca9",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "016ecccf9e73696a185f",
+    "url": "/blog/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c27c0ba1ee9ed18b1f47",
+    "url": "/blog/static/js/2.7eccd08f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blog/static/js/2.7eccd08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "016ecccf9e73696a185f",
+    "url": "/blog/static/js/main.d4bf63be.chunk.js"
+  },
+  {
+    "revision": "835233743d855939bee7",
+    "url": "/blog/static/js/runtime-main.10c1e3dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blog/static/media/logo.5d5d9eef.svg"
+  }
+]);
