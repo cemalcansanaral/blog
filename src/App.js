1a9fbf1d6695a4b129880e2d8cdb33fb3019c454
Cemal Can Şanaral <c.can.sanaral@gmail.com>
2020-03-30 17:20:39 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/navbar"
 
 function App() {
   return (
     <div className="App">
-      Cemal Can Şanaral
+      <Navbar/>
     </div>
   );
 }
